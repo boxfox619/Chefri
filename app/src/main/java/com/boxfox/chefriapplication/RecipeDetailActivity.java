295b81548f144eb947656efdcac203658d384ef6
boxfox <rlatjdfo112@naver.com>
2017-06-01 23:50:19 +0900

    recipe detail activity

--- app/src/main/java/com/boxfox/chefriapplication/RecipeDetailActivity.java
+++ app/src/main/java/com/boxfox/chefriapplication/RecipeDetailActivity.java
@@ -1,7 +1,10 @@
 package com.boxfox.chefriapplication;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 public class RecipeDetailActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class RecipeDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_detail);
+
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintEnabled(true);
+        tintManager.setNavigationBarTintEnabled(true);
+        tintManager.setTintColor(Color.parseColor("#20000000"));
+
     }
 }
