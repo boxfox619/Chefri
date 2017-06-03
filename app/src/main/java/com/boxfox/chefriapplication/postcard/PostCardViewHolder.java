9c8a7e4832414d818708dcf6425220b9283cbe94
boxfox <rlatjdfo112@naver.com>
2017-06-04 00:13:04 +0900

    Prepare for RecyclerView2

--- app/src/main/java/com/boxfox/chefriapplication/postcard/PostCardViewHolder.java
+++ app/src/main/java/com/boxfox/chefriapplication/postcard/PostCardViewHolder.java
@@ -1,9 +1,12 @@
 package com.boxfox.chefriapplication.postcard;
 
+import android.media.Image;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.androidquery.AQuery;
 import com.boxfox.chefriapplication.R;
 import com.pkmmte.view.CircularImageView;
 
@@ -14,6 +17,7 @@ import com.pkmmte.view.CircularImageView;
 public class PostCardViewHolder extends RecyclerView.ViewHolder {
     private View bgView;
     private CircularImageView profileImageView;
+    private ImageView bgImageView;
 
     private TextView tv_name, tv_info;
 
@@ -23,6 +27,7 @@ public class PostCardViewHolder extends RecyclerView.ViewHolder {
         profileImageView = (CircularImageView) itemView.findViewById(R.id.avatar);
         tv_name = (TextView) itemView.findViewById(R.id.tv_name);
         tv_info = (TextView) itemView.findViewById(R.id.tv_info);
+        bgImageView = (ImageView) itemView.findViewById(R.id.bg_image);
     }
 
     public void setName(String name){
@@ -34,7 +39,13 @@ public class PostCardViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void setProfileImage(String url){
+        AQuery aq = new AQuery(itemView.getContext());
+        aq.id(profileImageView).image(url, true, true, 0, R.drawable.user, null, AQuery.FADE_IN);
+    }
 
+    public void setBackgroundImage(String url){
+        AQuery aq = new AQuery(itemView.getContext());
+        aq.id(bgImageView).image(url, true, true, 0, R.drawable.user, null, AQuery.FADE_IN);
     }
 
     private void setRandomColor(){
