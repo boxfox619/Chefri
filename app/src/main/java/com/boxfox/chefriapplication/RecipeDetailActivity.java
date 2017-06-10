3fb160705497e062b81a939d24b2f03b10603b6d
rlatjdfo112 <rlatjdfo112@naver.com>
2017-06-10 22:41:00 +0900

    RecipeDetailActivity Modify

--- app/src/main/java/com/boxfox/chefriapplication/RecipeDetailActivity.java
+++ app/src/main/java/com/boxfox/chefriapplication/RecipeDetailActivity.java
@@ -5,6 +5,8 @@ import android.graphics.Color;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.androidquery.AQuery;
@@ -12,16 +14,27 @@ import com.androidquery.callback.AjaxCallback;
 import com.androidquery.callback.AjaxStatus;
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 public class RecipeDetailActivity extends AppCompatActivity {
     private ProgressDialog progressDialog;
+
+    @InjectView(R.id.recipe_name) TextView tv_recipeName;
+    @InjectView(R.id.recipe_materials) TextView tv_recipeMaterials;
+    @InjectView(R.id.recipe_lovers) TextView tv_recipeLovers;
+    @InjectView(R.id.cook_duration) TextView tv_cookDuration;
+    @InjectView(R.id.recipe_kcal) TextView tv_recipeKcal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_detail);
+        ButterKnife.inject(this);
         progressDialog = new ProgressDialog(RecipeDetailActivity.this);
 
         SystemBarTintManager tintManager = new SystemBarTintManager(this);
@@ -38,7 +51,15 @@ public class RecipeDetailActivity extends AppCompatActivity {
                 if (result != null) {
                     try {
                         JSONObject object = new JSONObject(result);
-
+                        tv_recipeName.setText(object.getString("Name"));
+                        JSONArray materials = object.getJSONArray("Materials");
+                        tv_recipeMaterials.setText(materials.getString(0));
+                        for(int i = 1; i < materials.length() ;i++){
+                            tv_recipeMaterials.append(", "+materials.getString(i));
+                        }
+                        tv_recipeKcal.setText(object.getString("Kcal"));
+                        tv_recipeLovers.setText(object.getString("Lovers"));
+                        tv_cookDuration.setText(object.getString("Duration"));
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
