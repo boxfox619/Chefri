e6611d165f65559d8b88d23f60dc8db313a0ac72
rlatjdfo112 <rlatjdfo112@naver.com>
2017-06-15 08:20:17 +0900

    develop the searchbar

--- app/src/main/java/com/boxfox/chefriapplication/MainActivity.java
+++ app/src/main/java/com/boxfox/chefriapplication/MainActivity.java
@@ -1,16 +1,14 @@
 package com.boxfox.chefriapplication;
 
-import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
-import android.util.Log;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.view.View;
 import android.widget.RelativeLayout;
 
+import com.boxfox.chefriapplication.databinding.ActivityMainBinding;
 import com.boxfox.chefriapplication.postcard.PostCardAdaptor;
 import com.boxfox.chefriapplication.postcard.PostCardItem;
 
@@ -19,21 +17,21 @@ public class MainActivity extends AppCompatActivity {
     private boolean isSearchShow;
 
     private PostCardAdaptor cardAdaptor;
-    private RelativeLayout searchLayout;
+    private View searchLayout;
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        startActivity(new Intent(MainActivity.this, RecipeWriteActivity.class));
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_recipe_write);
 
         isSearchShow = true;
-        searchLayout = (RelativeLayout) findViewById(R.id.searchLayout);
+        initalizeSearchBar();
         final RecyclerView cardListView = (RecyclerView) findViewById(R.id.recycler_view);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         cardListView.setLayoutManager(layoutManager);
-
         cardListView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
@@ -56,5 +54,27 @@ public class MainActivity extends AppCompatActivity {
         cardAdaptor.add(new PostCardItem(1, "test", "subInfo asdasd", 50, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJTkGxzhDu3iCYyt2Qg8Yi8LKy_vBK-uQwac2SRjwxCgA30Rujsw", "https://logopond.com/logos/685456e8a158ec3138b1eae0b6a321cc.png"));
         cardAdaptor.add(new PostCardItem(2, "test", "subInfo asdasd", 50, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJTkGxzhDu3iCYyt2Qg8Yi8LKy_vBK-uQwac2SRjwxCgA30Rujsw", "https://logopond.com/logos/685456e8a158ec3138b1eae0b6a321cc.png"));
         cardAdaptor.add(new PostCardItem(3, "test", "subInfo asdasd", 50, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJTkGxzhDu3iCYyt2Qg8Yi8LKy_vBK-uQwac2SRjwxCgA30Rujsw", "https://logopond.com/logos/685456e8a158ec3138b1eae0b6a321cc.png"));
+    }
+
+    private void initalizeSearchBar() {
+        searchLayout = binding.searchLayout;
+        searchLayout.findViewById(R.id.search_by_food).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        searchLayout.findViewById(R.id.search_by_tools).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        searchLayout.findViewById(R.id.search_by_kcal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/boxfox/chefriapplication/RecipeWriteActivity.java
+++ app/src/main/java/com/boxfox/chefriapplication/RecipeWriteActivity.java
@@ -5,22 +5,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Build;
-import android.os.ParcelFileDescriptor;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.util.TypedValue;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.androidquery.AQuery;
@@ -28,16 +18,10 @@ import com.androidquery.callback.AjaxCallback;
 import com.androidquery.callback.AjaxStatus;
 import com.boxfox.chefriapplication.databinding.ActivityRecipeWriteBinding;
 
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
-import java.io.FileDescriptor;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class RecipeWriteActivity extends AppCompatActivity {
